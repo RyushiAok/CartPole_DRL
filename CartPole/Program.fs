77203311f8a869788dae230bfd897914aaee783d
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2022-01-01 02:58:31 +0900

    akeome

--- CartPole/Program.fs
+++ CartPole/Program.fs
@@ -89,7 +89,7 @@ module Main =
                 //|> ignore 
             CartPole.Gui.appRun subject argv  
 
-        let pythonEnv () = 
+        let pythonEnvDuel () = 
             let client = 
                 let socket = 
                     let endPoint = IPEndPoint(IPAddress.Any, 8080)
@@ -120,7 +120,7 @@ module Main =
                         env.Reflect(action) |> ignore 
             } |> Async.RunSynchronously
 
-        let pythonEnv2 () =  
+        let pythonEnvA2C () =  
             async {
                 let envs = [|
                     for i in 0..15 ->
@@ -163,125 +163,11 @@ module Main =
                         envs[0].Reflect(action |> function | 0 -> Left | _ -> Right) |> ignore  
             }
             |> Async.RunSynchronously
-            
-
-        //let pythonEnv2 () = 
-        //    //async { 
-        //        let client = 
-        //            let socket = 
-        //                let endPoint = IPEndPoint(IPAddress.Any, 8080)
-        //                let socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
-        //                socket.Bind(endPoint)
-        //                socket.Listen(10)
-        //                socket 
-        //            socket.Accept() 
-        //        let steps = 200 
-        //        let clientMessage i = 
-        //            //printfn "%A" <| sprintf "^\%d+" i
-        //            let regexId = Regex(sprintf "^%d" i, RegexOptions.Compiled)
-        //            let regexObs = Regex("^\d+,o,(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+)$", RegexOptions.Compiled) 
-        //            let regexAct = Regex("^\d+,r,(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+),(-?\d+\.\d+)$", RegexOptions.Compiled)   
-        //            let (|Obs|Act|Non|)  (input:string) =
-        //                if regexId.Match(input).Success then 
-        //                    regexObs.Match(input) |> fun m -> 
-        //                        if m.Success then 
-        //                            Obs ( 
-        //                                m.Groups.Values 
-        //                                |> Seq.toArrayQuick 
-        //                                |> fun a -> a.[1..]
-        //                                |> Array.map(fun t -> t.ToString() |> System.Double.Parse) 
-        //                            )
-        //                        else 
-        //                            regexAct.Match(input) |> fun m -> 
-        //                                if m.Success then 
-        //                                    Act (
-        //                                        m.Groups.Values 
-        //                                        |> Seq.toArrayQuick 
-        //                                        |> fun a -> a.[1..]
-        //                                        |> Array.map(fun t -> t.ToString() |> System.Double.Parse)
-        //                                        |> fun a -> a.[0..3], a[4..] 
-        //                                    )
-        //                                else 
-        //                                    Non
-        //                else 
-        //                    Non 
-        //            let clientMsg = 
-        //                let buffer = Array.create 512 (byte 0) 
-        //                let rec loop () =  
-        //                    asyncSeq {
-        //                        let len = client.Receive(buffer) 
-        //                        let msg = Encoding.Default.GetString(buffer, 0, len)
-        //                        printfn "recv:%s" msg
-        //                        match msg with 
-        //                        | Obs obs -> 
-        //                            yield (ClientMessage.Observations obs )
-        //                            yield! loop ()
-        //                        | Act (obs,res) -> 
-        //                            yield (ClientMessage.ActionResult (obs, res[0], res[1]=1.))
-        //                            yield! loop ()
-        //                        | Non -> yield! loop ()  
-        //                    }  
-        //                loop () 
-        //            clientMsg 
-        //        let actorCritic =  
-        //            ActorCritic(
-        //                actor=ActorNetwork (observationSize=4,hiddenSize=32,actionCount=2),
-        //                critic=CriticNetwork(observationSize=4,hiddenSize=32),
-        //                learningRate=0.0025    // 0.01 0.003 
-        //            ) 
-        //        let envs = 
-        //            [| for i in 0..1 ->  
-        //                let cm = clientMessage i  
-        //                //cm
-        //                //|> AsyncSeq.subscribe(fun msg -> 
-        //                //    printfn "%A" msg
-        //                //)
-        //                //|> ignore
-        //                let cc = BehaviorSubject ClientCommand.Reset
-        //                cc 
-        //                |> Observable.subscribe (fun cmd -> 
-        //                     match cmd with 
-        //                     | ClientCommand.Reset ->
-        //                         sprintf "%d,reset" i 
-        //                         |> fun t -> printfn "%s" t ; t
-        //                         |> Encoding.Default.GetBytes 
-        //                         |> client.Send
-        //                         |> ignore
-        //                     | ClientCommand.Action str ->
-        //                        sprintf "%d,%s" i str 
-        //                        |> fun t -> printfn "%s" t ; t
-        //                        |> Encoding.Default.GetBytes 
-        //                        |> client.Send
-        //                        |> ignore 
-        //                     //printfn "%d,act" i
-        //                )
-        //                |> ignore
-        //                GymEnvironment'(
-        //                    cm,
-        //                    cc,
-        //                    steps) 
-        //            |] 
-        //        let agents =
-        //            envs  
-        //            |> Array.map(fun e -> 
-        //                ActorCriticAgent(
-        //                    actorCritic = actorCritic,
-        //                    env = e
-        //                )
-        //            ) 
-        //        A2C.optimize(actorCritic, agents) |> ignore 
-        //        let agent = ActorCriticAgent(actorCritic,envs[0]) 
-        //        while true do 
-        //            envs[0].Reset()
-        //            while not <| envs[0].Failed() do 
-        //                Thread.Sleep 20
-        //                let action = agent.SelectAction(dsharp.tensor <| [envs[0].Observations()]).toInt32()
-        //                envs[0].Reflect(action |> function | 0 -> Left | _ -> Right) |> ignore 
-            //} |> Async.RunSynchronously
+ 
         //fsharpEnv() 
-        //pythonEnv()
+        //pythonEnvDuel()
 
-        pythonEnv2 ()
+        pythonEnvA2C ()
 
         0
 
