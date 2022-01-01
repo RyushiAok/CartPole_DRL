a8395e0dbcc93ecf95199e6d88e3525d653b7ee3
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2022-01-02 03:11:38 +0900

    非同期で描画

--- cartpole_gym_environment/main.py
+++ cartpole_gym_environment/main.py
@@ -1,6 +1,7 @@
 # https://tonari-it.com/python-pycharm-package-install/
 import subprocess
 import time
+import threading
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,7 +12,18 @@ from multiprocessing import Pool
 import typing
 from JSAnimation.IPython_display import display_animation
 from matplotlib import animation
+import asyncio
 
+def render(env):
+    while True: env.render(mode='rgb_array')
+
+# https://kuttsun.blogspot.com/2020/03/python-fire-and-forget.html
+def fire_and_forget(task, *args, **kwargs):
+    loop = asyncio.get_event_loop()
+    if callable(task):
+        return loop.run_in_executor(None, task, *args, **kwargs)
+    else:
+        raise TypeError('Task must be a callable')
 
 def tcp_server(args):
     i, socket, env = args
@@ -19,10 +31,11 @@ def tcp_server(args):
     socket.bind(("127.0.0.1", 8080 + i))
     socket.listen(10)
     client, _ = socket.accept()
-
+    if i == 0:
+        fire_and_forget(render, env)
     print("connected %d" % i)
     while True:
-        if i == 0: env.render(mode='rgb_array')
+        # if i == 0: env.render(mode='rgb_array')
         msg = client.recv(1024).decode('ascii')
         if msg == 'reset':
             observation = env.reset()
@@ -41,10 +54,11 @@ def tcp_server(args):
 def gym_environment(n: int):
     sockets = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for i in range(n)]
     envs = [gym.make('CartPole-v0') for i in range(n)]
-    p = Pool(32)
+    p = Pool(n)
     values = [(i, sockets[i], envs[i]) for i in range(n)]
     p.map(tcp_server, values)
     p.close()
+
 
 
 if __name__ == '__main__':
